d308670db845769f883c18027d946f6266769430
Gael Rozario <gaelrozario@gmail.com>
2025-05-15 20:20:05 +0530

    adding mysql filetype to completion

--- nvim/.config/nvim/lua/gael/plugins/nvim-cmp.lua
+++ nvim/.config/nvim/lua/gael/plugins/nvim-cmp.lua
@@ -68,6 +68,18 @@ return {
         { name = "luasnip" }, -- snippets
         { name = "buffer"}, -- text within current buffer
         { name = "path" }, -- file system paths
+        { name = "codeium" }, -- file system paths
+      },
+    })
+    -- adding for vim-dadbod extra
+    cmp.setup.filetype({ "mysql" }, {
+      sources = {
+        { name = "vim-dadbod-completion" },
+        { name = "nvim_lsp"}, -- nvimlsp
+        { name = "luasnip" }, -- snippets
+        { name = "buffer"}, -- text within current buffer
+        { name = "path" }, -- file system paths
+        { name = "codeium" }, -- file system paths
       },
     })
   end,
